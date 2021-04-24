a0d2c8a87487a0962f866f8d9aab3b6fb77749f5
Ben Callis <ben@ivihouse.com>
2021-04-24 17:08:22 -0600

    added better loading structure and flow, custom splash screen configuration, more theme setup, pretty much ready to crank out the styles and screens

--- app/App.tsx
+++ app/App.tsx
@@ -7,36 +7,21 @@ import {
   SafeAreaProvider,
   initialWindowMetrics,
 } from 'react-native-safe-area-context';
-import {
-  NavigationContainerRef,
-  DefaultTheme,
-  DarkTheme,
-} from '@react-navigation/native';
 
 import {Appearance, AppearanceProvider} from 'react-native-appearance';
 import VersionNumber from 'react-native-version-number';
 
 // Enable react navigation screens before rendering any react-navigation configurations
 enableScreens();
-import {ThemeProvider as CAThemeProvider} from 'components/theme';
+import {ThemeProvider as CAThemeProvider, theme} from 'components/theme';
 import {LocalizationProvider} from './components/TranslationProvider';
 import {RootNavigator} from './Navigation';
 import {UserProvider} from './UserContext';
 import Toast from 'react-native-toast-message';
-import {
-  canExit,
-  setRootNavigation,
-  useBackButtonHandler,
-  useNavigationPersistence,
-} from './navUtilities';
-import * as storage from './utils';
 
 const defaultTheme = Appearance.getColorScheme() || 'light';
-const NAVIGATION_PERSISTENCE_KEY = `NAVIGATION_STATE_KEY-${VersionNumber}`;
 
 export default function App() {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const navigationRef = React.useRef<NavigationContainerRef>();
   const [themeState, setThemeState] = React.useState(defaultTheme);
 
   React.useEffect(() => {
@@ -46,55 +31,32 @@ export default function App() {
     });
     return () => subscription.remove();
   }, []);
+
   const currentThemeState = React.useMemo(() => ({themeState}), [
     themeState,
     setThemeState,
   ]);
+
   StatusBar.setBarStyle(
     currentThemeState.themeState === 'dark' ? 'light-content' : 'dark-content',
     true,
   );
 
-  setRootNavigation(navigationRef);
-  useBackButtonHandler(navigationRef, canExit);
-
-  const {onNavigationStateChange} = useNavigationPersistence(
-    storage,
-    NAVIGATION_PERSISTENCE_KEY,
-  );
-
-  const navigatorTheme = {
-    ...DefaultTheme,
-    colors: {
-      ...DefaultTheme.colors,
-      // prevent layout blinking when performing navigation
-      background: 'white',
-      border: 'transparent',
-      card: 'white',
-    },
-  };
   return (
-    <CAThemeProvider>
-      <ThemeProvider>
-        <LocalizationProvider>
-          <UserProvider>
-            <SafeAreaProvider initialMetrics={initialWindowMetrics}>
-              <StatusBar />
-              <RootNavigator
-                ref={navigationRef}
-                initialState={navHistory}
-                onStateChange={onNavigationStateChange}
-                theme={
-                  currentThemeState.themeState === 'dark'
-                    ? DarkTheme
-                    : navigatorTheme
-                }
-              />
-              <Toast ref={(ref) => Toast.setRef(ref)} />
-            </SafeAreaProvider>
-          </UserProvider>
-        </LocalizationProvider>
-      </ThemeProvider>
-    </CAThemeProvider>
+    <AppearanceProvider>
+      <CAThemeProvider>
+        <ThemeProvider>
+          <LocalizationProvider>
+            <UserProvider>
+              <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+                <StatusBar />
+                <RootNavigator theme={currentThemeState} />
+                <Toast ref={(ref) => Toast.setRef(ref)} />
+              </SafeAreaProvider>
+            </UserProvider>
+          </LocalizationProvider>
+        </ThemeProvider>
+      </CAThemeProvider>
+    </AppearanceProvider>
   );
 }
--- app/Navigation.tsx
+++ app/Navigation.tsx
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, {
+  ReactElement,
+  useCallback,
+  useEffect,
+  useState,
+  useMemo,
+} from 'react';
 import {
+  DarkTheme,
+  DefaultTheme,
   NavigationContainer,
   NavigationContainerRef,
+  InitialState,
 } from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import FeatherIcons from 'react-native-vector-icons/Feather';
-import {View} from 'react-native';
+import {StatusBar, View} from 'react-native';
+import {Asset} from 'expo-asset';
+import * as Font from 'expo-font';
+import Constants from 'expo-constants';
 
-import Home from './screens/Home';
-import GroupDetail, {GroupDetailRouterProps} from './screens/GroupDetail';
-import {useTranslations} from './components/TranslationProvider';
-import GroupsAll from './screens/GroupsAll';
-import PostCreate, {PostCreateRouterProps} from './screens/PostCreate';
-import PostDetail, {PostDetailRouterProps} from './screens/PostDetail';
+import Home from 'screens/Home';
+import GroupDetail, {GroupDetailRouterProps} from 'screens/GroupDetail';
+import {useTranslations} from 'components/TranslationProvider';
+import GroupsAll from 'screens/GroupsAll';
+import PostCreate, {PostCreateRouterProps} from 'screens/PostCreate';
+import PostDetail, {PostDetailRouterProps} from 'screens/PostDetail';
 import Profile from 'screens/Profile';
+
+import {Appearance} from 'react-native-appearance';
+
+import {load, save} from './utils';
+
+const NAVIGATION_STATE_KEY = `NAVIGATION_STATE_KEY-${Constants.manifest.sdkVersion}`;
+const defaultTheme = Appearance.getColorScheme() || 'light';
 
 type HomeStackParams = {
   Home: undefined;
@@ -70,37 +89,147 @@ function ProfileStackNavigator() {
     </ProfileStack.Navigator>
   );
 }
+interface NavProps extends NavigationContainerRef {
+  theme: string | 'light';
+}
 
-export const RootNavigator = React.forwardRef<
-  NavigationContainerRef,
-  Partial<React.ComponentProps<typeof NavigationContainer>>
->((props, ref) => {
-  const translations = useTranslations();
+export type FontSource = Parameters<typeof Font.loadAsync>[0];
+const usePromiseAll = (
+  promises: Promise<void | void[] | Asset[]>[],
+  cb: () => void,
+) =>
+  useEffect(() => {
+    (async () => {
+      await Promise.all(promises);
+      cb();
+    })();
+  });
+
+const useLoadAssets = (assets: number[], fonts: FontSource): boolean => {
+  const [ready, setReady] = useState(false);
+  usePromiseAll(
+    [Font.loadAsync(fonts), ...assets.map((asset) => Asset.loadAsync(asset))],
+    () => setReady(true),
+  );
+  return ready;
+};
+
+interface LoadAssetsProps {
+  fonts?: FontSource;
+  assets?: number[];
+  children: ReactElement | ReactElement[];
+}
+// export const RootNavigator = React.forwardRef<
+//   NavProps,
+//   Partial<React.ComponentProps<typeof NavigationContainer>>
+// >((props, ref) => {
+//   const translations = useTranslations();
+//   const navigationRef = React.useRef<NavigationContainerRef>();
+//
+//   setRootNavigation(navigationRef);
+//   useBackButtonHandler(navigationRef, canExit);
+//
+//   const {onNavigationStateChange} = useNavigationPersistence(
+//     storage,
+//     NAVIGATION_PERSISTENCE_KEY,
+//   );
+//
+//   const navigatorTheme = {
+//     ...DefaultTheme,
+//     colors: {
+//       ...DefaultTheme.colors,
+//       // prevent layout blinking when performing navigation
+//       background: 'white',
+//       border: 'transparent',
+//       card: 'white',
+//     },
+//   };
+//
+//   return (
+//     <NavigationContainer
+//       theme={props.theme.themeState === 'dark' ? DarkTheme : navigatorTheme}
+//       ref={ref}>
+//       <MainTabs.Navigator>
+//         <MainTabs.Screen
+//           name="HomeStackNavigator"
+//           component={HomeStackNavigator}
+//           options={{
+//             title: translations.homeTabTitle,
+//             tabBarIcon: (props) => {
+//               return <FeatherIcons name="home" {...props} />;
+//             },
+//           }}
+//         />
+//         <MainTabs.Screen
+//           name="ProfileStackNavigator"
+//           component={ProfileStackNavigator}
+//           options={{
+//             title: translations.profileTabTitle,
+//             tabBarIcon: (props) => {
+//               return <FeatherIcons name="user" {...props} />;
+//             },
+//           }}
+//         />
+//       </MainTabs.Navigator>
+//     </NavigationContainer>
+//   );
+// });
+
+interface RootNavigatorProps {
+  fonts?: FontSource;
+  assets?: number[];
+  children: ReactElement | ReactElement[];
+}
+const RootNavigator = ({assets, fonts, children}: LoadAssetsProps) => {
+  const [isNavigationReady, setIsNavigationReady] = useState(!__DEV__);
+  const [themeState, setThemeState] = React.useState(defaultTheme);
+
+  const [initialState, setInitialState] = useState<InitialState | undefined>();
+  const ready = useLoadAssets(assets || [], fonts || {});
+
+  React.useEffect(() => {
+    const subscription = Appearance.addChangeListener(({colorScheme}) => {
+      // console.tron.log("THE COLOR SCHEME", colorScheme)
+      setThemeState(colorScheme);
+    });
+    return () => subscription.remove();
+  }, []);
+
+  useEffect(() => {
+    const restoreState = async () => {
+      try {
+        const savedStateString = await load(NAVIGATION_STATE_KEY);
+        const state = savedStateString
+          ? JSON.parse(savedStateString)
+          : undefined;
+        setInitialState(state);
+      } finally {
+        setIsNavigationReady(true);
+      }
+    };
+
+    if (!isNavigationReady) {
+      restoreState();
+    }
+  }, [isNavigationReady]);
+
+  const currentThemeState = useMemo(() => ({themeState}), [
+    themeState,
+    setThemeState,
+  ]);
+
+  const onStateChange = useCallback(
+    (state) => save(NAVIGATION_STATE_KEY, JSON.stringify(state)),
+    [],
+  );
+  if (!ready || !isNavigationReady) {
+    return <AppLoading />;
+  }
 
   return (
-    <NavigationContainer {...props} ref={ref}>
-      <MainTabs.Navigator>
-        <MainTabs.Screen
-          name="HomeStackNavigator"
-          component={HomeStackNavigator}
-          options={{
-            title: translations.homeTabTitle,
-            tabBarIcon: (props) => {
-              return <FeatherIcons name="home" {...props} />;
-            },
-          }}
-        />
-        <MainTabs.Screen
-          name="ProfileStackNavigator"
-          component={ProfileStackNavigator}
-          options={{
-            title: translations.profileTabTitle,
-            tabBarIcon: (props) => {
-              return <FeatherIcons name="user" {...props} />;
-            },
-          }}
-        />
-      </MainTabs.Navigator>
+    <NavigationContainer {...{onStateChange, initialState}}>
+      <StatusBar />
+      {children}
     </NavigationContainer>
   );
-});
+};
